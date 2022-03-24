3e190704c92b8fa097045ae2b2388c1ac07e041b
Roy <J.jyun0525@gmail.com>
2022-03-24 12:48:13 +0900

    로그인 화면 기본 구조를 작업

--- app/src/main/java/com/example/colosseum_yun/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum_yun/utils/ServerUtil.kt
@@ -2,4 +2,17 @@ package com.example.colosseum_yun.utils
 
 class ServerUtil {
 //    어떤 내용을 작성해야 서버 연결 전달 되는지 알아보자
+
+
+    companion object{
+
+//        모든기능의 기본이 되는 주소
+
+        val BASE_URI = " http://54.180.52.26"
+
+        fun postRequestLogin() {
+
+        }
+
+    }
 }
