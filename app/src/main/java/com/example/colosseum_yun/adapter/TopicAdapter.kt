14ba9e68af572d8c1edf7bfe275893b2914c69f1
Roy <J.jyun0525@gmail.com>
2022-03-30 16:44:59 +0900

    어댑터 추가

---
+++ app/src/main/java/com/example/colosseum_yun/adapter/TopicAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.colosseum_yun.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.colosseum_yun.R
+import com.example.colosseum_yun.datas.Topic
+
+class TopicAdapter(
+        val mContext : Context,
+        resId : Int,
+        val mList : List<Topic>) : ArrayAdapter<Topic>(mContext, resId, mList) {
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.topic_list_item, null)
+        }
+        val row = tempRow!!
+
+        return  row
+    }
+
+}
