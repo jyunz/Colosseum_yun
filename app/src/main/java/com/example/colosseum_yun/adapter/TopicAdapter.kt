f8df58c30c4e0db2f106abcd4391b2b4d991b523
Roy <J.jyun0525@gmail.com>
2022-04-01 15:34:41 +0900

    Glide 등 활용 -> 실제 토론 주제 목록 데이터 표시

--- app/src/main/java/com/example/colosseum_yun/adapter/TopicAdapter.kt
+++ app/src/main/java/com/example/colosseum_yun/adapter/TopicAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.colosseum_yun.R
 import com.example.colosseum_yun.datas.Topic
 
@@ -21,6 +24,16 @@ class TopicAdapter(
             tempRow = mInflater.inflate(R.layout.topic_list_item, null)
         }
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val topicImg = row.findViewById<ImageView>(R.id.topicImg)
+        val  topicTitleTxt = row.findViewById<TextView>(R.id.topicTitleTxt)
+
+        topicTitleTxt.text = data.title
+//        서버가 주는 URL => 그림 이미지에 표시 : Glide 라이브러리
+
+        Glide.with(mContext).load(data.imageURL).into(topicImg)
 
         return  row
     }
