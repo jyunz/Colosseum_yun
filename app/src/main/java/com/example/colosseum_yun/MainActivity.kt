2c69d2783bc207f3f014e5c7a1241e0c43c0d2cb
Roy <J.jyun0525@gmail.com>
2022-04-01 14:58:05 +0900

    JSONObject 재료로  => Topic으로 변환해주는 함수 추가

--- app/src/main/java/com/example/colosseum_yun/MainActivity.kt
+++ app/src/main/java/com/example/colosseum_yun/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.colosseum_yun
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.colosseum_yun.datas.Topic
+import com.example.colosseum_yun.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -16,6 +18,7 @@ class MainActivity : BaseActivity() {
         setValues()
 
     }
+
     override fun setupEvents() {
     }
 
@@ -27,6 +30,27 @@ class MainActivity : BaseActivity() {
     fun getTopicListFromServer() {
 
 //        서버에서 주제 목록을 받아오자. => /v2/mail_info => ServerUtil에 기능 추가 필요.
+        ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.Companion.JsonResponseHandler {
+            override fun onResponse(jsonObj: JSONObject) {
+
+//                서버에서 주제목록을 받아온 상황
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicsArr = dataObj.getJSONArray("topics")
+
+//            topicsArr 안에 있는 여러개의 주제들을 반.복.적.으로 파싱. => for문 활용
+//                배열에 10개 주제  : Index ->  0~9까지
+//                     또는 15개다 :           0~14 까지
+
+                for (index in 0 until topicsArr.length() ) {
+//                    index 위치에 맞는 주제들을  Topic 클래스 형태로 변환
+
+//                    json 파싱
+
+//                      mTopicList에 추가
+
+            }
+
+        })
     }
 
 
--- app/src/main/java/com/example/colosseum_yun/datas/Topic.kt
+++ app/src/main/java/com/example/colosseum_yun/datas/Topic.kt
@@ -1,8 +1,27 @@
 package com.example.colosseum_yun.datas
 
+import org.json.JSONObject
+
 class Topic {
 
     var id = 0     // 나중에 int 가 들어올것이라는 명시
    var title = ""  // 나중에 string 이 들어올 것이라는 명시
     var imageURL = ""
+
+    companion object {
+
+//        적당한 JSONObj(모양) 하나를 넣어주면 => Topic 형태로 변환해주는 함수 작성.
+
+        fun  getTopicDataFromJson (jsonObj : JSONObject) : Topic {
+            val resultTopic = Topic()
+
+            resultTopic.id = jsonObj.getInt("id")
+            resultTopic.title = jsonObj.getString("title")
+            resultTopic.imageURL = jsonObj.getString("img_url")
+//            = 은  오른쪽에 내용을 왼쪽에 넣어주라.복사해준다.
+//            오른쪽은 서버에서 받아낸것. 왼쪽은 실제로 기록을 해 줄 변수.
+
+            return resultTopic
+        }
+    }
 }
