965cbe89be32bcddac6de436696e77ae69a973cd
Roy <J.jyun0525@gmail.com>
2022-04-05 12:18:00 +0900

    의견의 내용만 실제 반영

--- app/src/main/java/com/example/colosseum_yun/adapter/ReplyAdapter.kt
+++ app/src/main/java/com/example/colosseum_yun/adapter/ReplyAdapter.kt
@@ -29,6 +29,13 @@ class ReplyAdapter(
 
         val data = mList[position]
 
+        val selectedSideTxt = row.findViewById<TextView>(R.id.selectedSideTxt)
+        val userNicknameTxt = row.findViewById<TextView>(R.id.userNicknameTxt)
+        val contentTxt = row.findViewById<TextView>(R.id.contentTxt)
+
+        contentTxt.text = data.content
+
+
 
         return  row
     }
