5ce03a0dde2537b82a41698e98598e21593799e8
Roy <J.jyun0525@gmail.com>
2022-04-05 16:06:16 +0900

    의견 파싱할때 -> 선택된 진영이 어딘지도 같이 파싱

--- app/src/main/java/com/example/colosseum_yun/datas/Reply.kt
+++ app/src/main/java/com/example/colosseum_yun/datas/Reply.kt
@@ -6,6 +6,8 @@ class Reply {
     var id = 0
     var content = ""
 
+    lateinit var selectedSide: Side
+
     companion object {
 
         fun getReplyFromJson(jsonObject: JSONObject) : Reply {
@@ -13,6 +15,9 @@ class Reply {
 
             resultReply.id = jsonObject.getInt("id")
             resultReply.content = jsonObject.getString("content")
+
+//            요부분 다시 잘 들을것.
+            resultReply.selectedSide = Side.getSideFromJson(jsonObject.getJSONObject("selected_side"))
 
             return resultReply
         }
