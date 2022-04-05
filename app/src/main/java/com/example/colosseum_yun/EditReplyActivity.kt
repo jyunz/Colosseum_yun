b227a42db2f97b46b94f5a029eed528860dc44a4
Roy <J.jyun0525@gmail.com>
2022-04-05 15:51:31 +0900

    의견 작성하기 기능 마무리

--- app/src/main/java/com/example/colosseum_yun/EditReplyActivity.kt
+++ app/src/main/java/com/example/colosseum_yun/EditReplyActivity.kt
@@ -2,8 +2,15 @@ package com.example.colosseum_yun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.colosseum_yun.datas.Side
+import com.example.colosseum_yun.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_edit_reply.*
+import org.json.JSONObject
 
 class EditReplyActivity : BaseActivity() {
+
+    lateinit var mSelectedSide : Side
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +22,41 @@ class EditReplyActivity : BaseActivity() {
     }
     override fun setupEvents() {
 
+        okBtn.setOnClickListener {
+
+            val inputContent = contentEdt.text.toString()
+
+            ServerUtil.postRequestReply(mContext, mSelectedSide.topicId, inputContent, object : ServerUtil.Companion.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    val  code = jsonObj.getInt("code")
+
+                    if (code == 200) {
+                        runOnUiThread {
+                            Toast.makeText(mContext, "의견등록에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+
+                    }
+                    else {
+                        val message = jsonObj.getString("message")
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            })
+        }
+
     }
 
     override fun setValues() {
+
+        mSelectedSide = intent.getSerializableExtra("mySide") as Side
+
+        mySelectedSideTxt.text = mSelectedSide.title
+
+
 
     }
 
