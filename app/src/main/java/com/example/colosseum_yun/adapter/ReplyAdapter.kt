69086bd352e405661e8a8edf91bcab94817390b4
Roy <J.jyun0525@gmail.com>
2022-04-06 14:29:47 +0900

    좋아요 싫어요 실제전송 ㄴ 나는 의견 목록이 안떠서 확인이 불가능

--- app/src/main/java/com/example/colosseum_yun/adapter/ReplyAdapter.kt
+++ app/src/main/java/com/example/colosseum_yun/adapter/ReplyAdapter.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.colosseum_yun.R
 import com.example.colosseum_yun.datas.Reply
 import com.example.colosseum_yun.datas.Topic
+import com.example.colosseum_yun.utils.ServerUtil
+import org.json.JSONObject
 
 class ReplyAdapter(
         val mContext : Context,
@@ -44,6 +47,29 @@ class ReplyAdapter(
 
         likeCountBtn.text = "좋아요 ${data.likeCount}개"
         disLikeCountBtn.text = "싫어요 ${data.dislikeCount}개"
+        
+//        좋아요 API호출
+        
+        likeCountBtn.setOnClickListener {
+//            Toast.makeText(mContext, "좋아요 연습", Toast.LENGTH_SHORT).show()
+//             ㄴ 연습해서 화면에 뜨면 서버유틸에서 작업. 작업후 ㄱ
+            ServerUtil.postRequestLikeDislike(mContext, data.id, true, object : ServerUtil.Companion.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                }
+            })
+
+        }
+
+//         싫어요 API호출
+        
+        disLikeCountBtn.setOnClickListener {
+            ServerUtil.postRequestLikeDislike(mContext, data.id, false, object : ServerUtil.Companion.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                }
+            })
+        }
 
 
 
--- app/src/main/java/com/example/colosseum_yun/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum_yun/utils/ServerUtil.kt
@@ -400,5 +400,58 @@ class ServerUtil {
 
             })
         }
+
+//       의견에 좋아요 / 싫어요 찍기
+        fun postRequestLikeDislike(context: Context, replyId : Int, isLike : Boolean, handler: JsonResponseHandler?) {
+
+
+    val urlString = "${BASE_URI}/topic_reply_like"
+
+    val formData = FormBody.Builder()
+        .add("reply_id", replyId.toString())
+        .add("is_like", isLike.toString())
+        .build()
+
+//            어디로 => 어떻게 => 어떤 데이터를 들고 가는지를 모두 종합해둔 ,  Request 변수 생성.
+    val request = Request.Builder()
+        .url(urlString)
+        .post(formData)
+        .header("X-Http-Token", ContextUtil.getToken(context))
+        .build()
+
+//            클라이언트로써의 동작. : Request 요청 실행  => OkHttp 라이브러리 지원.
+
+    val client = OkHttpClient()
+
+//            실제로 서버에 요청 날리기. = > 갔다 와서는 뭘 할건지?/할 일은 {} 안에 있음
+
+    client.newCall(request).enqueue(object : Callback {
+        override fun onFailure(call: Call, e: IOException) {
+//                        서버 연결 자체를 실패 한 경우(서버 마비, 인터넷 단선)
+        }
+
+        override fun onResponse(call: Call, response: Response) {
+
+//                        로그인 성공, 로그인 실패 (연결 성공 - > 검사실패)
+//                        응답이 돌아온 경우
+
+
+//                        응답 본문을 String으로 저장
+            val bodyString = response.body!!.string()
+
+//                        그런데 나오는 로그캣이 bodyString 변수에는 한글이 깨져 나오기 때문에
+//                        JSONObject로 변환하면, 한글이 정상적으로 처리됨
+
+            val jsonObj = JSONObject(bodyString)
+
+            Log.d("응답본문", jsonObj.toString())
+
+//                        handler 변수가 null이 아니라면 ,(실체가 있다면)
+//                        그 내부에 적힌 내용 실행.
+            handler?.onResponse(jsonObj)
+        }
+
+    })
+}
     }
 }
