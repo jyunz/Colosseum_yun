41dda0e8078b2126c9cd53d8f2bbdb769c85c407
Roy <J.jyun0525@gmail.com>
2022-04-07 10:35:53 +0900

    상속 이용 -> 뒤로 가기 기능 일괄 부여

--- app/src/main/java/com/example/colosseum_yun/BaseActivity.kt
+++ app/src/main/java/com/example/colosseum_yun/BaseActivity.kt
@@ -1,11 +1,14 @@
 package com.example.colosseum_yun
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity :AppCompatActivity() {
+
+    lateinit var backBtn : ImageView
 
     val mContext = this
 
@@ -32,6 +35,12 @@ abstract class BaseActivity :AppCompatActivity() {
 //        양 옆 여백 제거
         val myToolbar = defaultActionBar.customView.parent as Toolbar
         myToolbar.setContentInsetsAbsolute(0,0)
+
+        backBtn = defaultActionBar.customView.findViewById(R.id.backBtn)
+
+        backBtn.setOnClickListener {
+            finish()
+        }
 
     }
 
